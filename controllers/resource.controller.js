82c628e7f63d7a54c9ae6d28b48348a234589eda
Stefy <stefany152005@gmail.com>
2026-06-18 19:06:55 -0500

    feat: simular endpoints independientes para service-alpha y service-beta

--- controllers/resource.controller.js
+++ controllers/resource.controller.js
@@ -3,17 +3,21 @@ export class ResourceController {
      * Simula un recurso privado del Microservicio Alpha.
      */
     static getAlphaPrivateData(req, res) {
-        // TODO: Implementar respuesta para Microservicio Alpha.
-        // 1. Responder con un mensaje que indique acceso exitoso al Servicio Alpha.
-        // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        return res.json({
+            service: 'service-alpha',
+            message: 'Acceso concedido al recurso privado del Servicio Alpha',
+            authenticated_user: req.user
+        });
     }
 
     /**
      * Simula un recurso privado del Microservicio Beta.
      */
     static getBetaPrivateData(req, res) {
-        // TODO: Implementar respuesta para Microservicio Beta.
-        // 1. Responder con un mensaje que indique acceso exitoso al Servicio Beta.
-        // 2. Incluir datos del usuario autenticado (req.user) en la respuesta.
+        return res.json({
+            service: 'service-beta',
+            message: 'Acceso concedido al recurso privado del Servicio Beta',
+            authenticated_user: req.user
+        });
     }
 }
